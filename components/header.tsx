3c875f50a81c5fd487bc05f98420f610b94b6c06
Hamrless04 <devadathks004@gmail.com>
2024-06-30 15:03:24 +0530

    first commit

--- components/header.tsx
+++ components/header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
       <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
 
         {links.map((link) => (
-          <li key={link.hash}>
+          <li className="" key={link.hash}>
             <Link href={link.hash}>{link.name}</Link>
           </li>
         ))}
