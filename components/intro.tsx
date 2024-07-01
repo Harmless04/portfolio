8d51a61ebcc8299cceba4f60605d1ef782bc5f09
Hamrless04 <devadathks004@gmail.com>
2024-07-01 13:35:59 +0530

    intro section text added , image added and emoji added and aligned

---
+++ components/intro.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from 'next/image';
+import React from 'react';
+import {motion} from 'framer-motion';
+
+
+export default function  Intro() {
+  return (
+    <section className=" mb-28 max-w-[50rem]
+    text-center sm:mb-0">
+    <div className='flex items-center justify-center'>
+        <div className='relative'>
+        <motion.div
+            initial={{opacity:0, scale:0}}
+            animate={{opacity:1, scale:1}}
+            transition={{type:"tween",duration:0.2, }}
+        >
+            <Image src="https://litt.citizendigitalfoundation.org/files/1684164085021.jpeg" alt="Devadath Portrait" width="192"
+            height="192"
+            quality="95"
+            priority={true}
+            className='h-24 w-24 rounded-full
+            object-cover
+            border-[0.35rem] border-white shadow-xl'
+             />
+             </motion.div>
+             <motion.span className=' absolute text-4xl bottom-0 right-0 '
+             initial={{opacity:0,scale:0}}
+             animate={{opacity:1, scale:1}}
+             transition={{type:"spring", 
+                 stiffness:125,
+                 delay:0.1,
+                 duration:0.7  
+             }}
+             
+             >
+                🎯
+             </motion.span>
+        </div>
+    </div>
+    <p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
+    <span className="font-bold">Hello, I'm Devadath.K.S.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> who's{" "}
+        <span className="font-bold">into the Web3 world  </span> & AI. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is on{" "}
+        <span className="underline">gaining knowledge wherever i go</span>
+
+    </p>
+ </section>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images: {
+        remotePatterns  : [
+            {
+                protocol : 'https',
+                hostname:'litt.citizendigitalfoundation.org',
+            },
+        ],
+},
+};
 module.exports = nextConfig
+ 
